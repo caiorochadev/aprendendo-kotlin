b11b7d2249cedc558bb3f944f567a9330347b972
Caio Rocha <caiorochadev@outlook.com>
2023-07-31 19:07:51 -0300

    Update Array, count, find and any

--- src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
+++ src/main/kotlin/one/digitalinnovation/collections/TesteOperacoes.kt
@@ -3,6 +3,8 @@ package one.digitalinnovation.collections
 fun main() {
     val salarios = doubleArrayOf(1000.0 , 2250.0 , 4000.0, 6000.0)
 
+
+
     for (salario in salarios){ println(salario)}
     println("---")
 
@@ -13,4 +15,14 @@ fun main() {
     val salariosMaiorQue2500 = salarios.filter { it > 2500 }
     println("---")
     salariosMaiorQue2500.forEach { println(it) }
+
+    println("uso do count")
+    println(salarios.count {it in 1000.0 .. 5000.0})
+    println("uso do find")
+    println(salarios.find {it == 2250.0 })
+    println("uso do any")
+    println(salarios.any {it == 1000.0 })
+
+
+
 }
